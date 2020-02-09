3da66f02d6aea1e6922ffb81038a7bb1a2b055d6
Troy Martin <beef.erikson.studios@gmail.com>
2020-02-09 17:07:00 -0500

    Changed GetDate a little

--- Input Output and Calculations/GetDate/GetDate/Source.cpp
+++ Input Output and Calculations/GetDate/GetDate/Source.cpp
@@ -6,6 +6,5 @@ int main()
 	int day, month, year;
 	cout << "Enter day, month, and year (dd mm yyyy): ";
 	scanf_s("%d %d %d", &day, &month, &year);
-	cout << "The date is day: " << day << " month: " <<
-		month << " year: " << year << endl;
+	cout << "The date is: " << day << '/' << month << '/' << year << endl;
 }
