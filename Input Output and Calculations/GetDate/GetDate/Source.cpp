7562686a4ccf6f600a4421e5e96c4f180d329bb2
Troy Martin <beef.erikson.studios@gmail.com>
2020-02-09 16:47:33 -0500

    Added GetDate project

---
+++ Input Output and Calculations/GetDate/GetDate/Source.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int day, month, year;
+	cout << "Enter day, month, and year (dd mm yyyy): ";
+	scanf_s("%d %d %d", &day, &month, &year);
+	cout << "The date is day: " << day << " month: " <<
+		month << " year: " << year << endl;
+}
