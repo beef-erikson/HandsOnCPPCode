5b26e55914e3b321c7e5ceaeff7e08eb4ecde8f0
Troy Martin <beef.erikson.studios@gmail.com>
2020-02-13 14:50:03 -0500

    BMI updated to output based on BMI value

--- Input Output and Calculations/BMI/BMI/Source.cpp
+++ Input Output and Calculations/BMI/BMI/Source.cpp
@@ -4,6 +4,7 @@ using namespace std;
 int main()
 {
 	int age, weight, height;
+	int BMI{};
 
 	cout << "Enter age: ";
 	cin >> age;
@@ -12,6 +13,15 @@ int main()
 	cout << "Enter height in inches: ";
 	cin >> height;
 	
-	cout << endl << "Your BMI is: " << weight * 705 / height / height << endl;
-	cout << "A BMI over 25 is considered overweight." << endl;
+	BMI = weight * 705 / height / height;
+	cout << endl << "Your BMI is: " << BMI << endl;
+	
+	if (BMI < 18.5)
+		cout << "You are underweight\n";
+	if (BMI >= 18.5 && BMI < 25)
+		cout << "You are a normal weight\n";
+	if (BMI >= 25 && BMI < 30)
+		cout << "You are overweight\n";
+	if (BMI > 30)
+		cout << "You are obese\n";
 }
