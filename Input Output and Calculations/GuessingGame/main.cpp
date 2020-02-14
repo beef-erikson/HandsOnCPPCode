4056184c9ade033d70748dd8c827cb3e8dc47d9d
Troy Martin <beef.erikson.studios@gmail.com>
2020-02-14 13:59:57 -0500

    GuessingGame updated with do/while loop.

--- Input Output and Calculations/GuessingGame/main.cpp
+++ Input Output and Calculations/GuessingGame/main.cpp
@@ -1,25 +1,37 @@
 #include <iostream>
 #include "cstdlib"
 #include "ctime"
+
 using namespace std;
 
 int main() {
     int number{}, guess{};
+    int numberOfGuesses{0};
 
     // Generates random between 1 and 10
-    srand(static_cast<unsigned>(time(0)));
+    srand(static_cast<unsigned>(time(nullptr)));
     number = rand() % 10 + 1;
 
     // Gets user's guess and display answer
-    cout << "Guess the number (1 - 10): ";
-    cin >> guess;
-    cout << "The number was " << number << endl;
-    if (guess < number)
-        cout << "Your guess was too low with " << guess << "." << endl;
-    else if (guess > number)
-        cout << "Your guess was too high with " << guess << "." << endl;
-    else
-        cout << "You guessed correctly with " << guess << "!" << endl;
+    do {
+        cout << "Guess the number (1 - 10): ";
+        cin >> guess;
+
+        if (guess < 1 || guess > 10)
+            cout << "Number is out of range. Guess 1 to 10.\n";
+        else if (guess < number) {
+            numberOfGuesses++;
+            cout << "Your guess was too low with " << guess << "." << endl;
+        }
+        else if (guess > number) {
+            numberOfGuesses++;
+            cout << "Your guess was too high with " << guess << "." << endl;
+        }
+        else
+            cout << "\nYou guessed correctly with " << guess << "!" << endl;
+    } while (guess != number);
+
+    cout << "Congrats! It took you " << numberOfGuesses << " guesses!\n";
 
     return 0;
 }
