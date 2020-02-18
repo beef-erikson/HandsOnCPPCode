318a375a36100bb917b7a2e8ea7642e8e2469064
Troy Martin <beef.erikson.studios@gmail.com>
2020-02-17 21:13:08 -0500

    Arrays01 modified with a const for element size of Array.

--- Arrays/Arrays01/main.cpp
+++ Arrays/Arrays01/main.cpp
@@ -2,17 +2,18 @@
 
 using namespace std;
 
-// Reads in 5 numbers into an array and outputs the values using loops
+// Reads in numbers into an array and outputs the values using loops
 int main() {
-    int numbers[5];
+    const int SIZE {7};     // Change this to alter array size
+    int numbers[SIZE];
 
-    for (int i{0}; i < 5; i++) {
+    for (int i{0}; i < SIZE; i++) {
         cout << "Enter a number: ";
         cin >> numbers[i];
     }
 
     cout << endl;
-    for (int i{0}; i < 5; i++) {
+    for (int i{0}; i < SIZE; i++) {
         cout << "The number in array element " << i << " is " << numbers[i]
              << endl;
     }
