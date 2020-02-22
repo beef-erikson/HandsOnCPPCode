b1f37a762e9d76635e84e525aaae5fb5cb255946
Troy Martin <beef.erikson.studios@gmail.com>
2020-02-22 07:44:16 -0500

    Strings added

---
+++ Arrays/Strings/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    char sentence[41];
+
+    cout << "Enter a sentence: ";
+    gets(sentence);
+
+    cout << "Your sentence was:\n" << sentence;
+
+
+    return 0;
+}
