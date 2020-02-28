b2969f4b8b297e349d02cc6fce51e7ffe396fc79
Troy Martin <beef.erikson.studios@gmail.com>
2020-02-28 16:20:23 -0500

    AddressOf updated with pointers

--- PointersAndReferences/AddressOf/main.cpp
+++ PointersAndReferences/AddressOf/main.cpp
@@ -3,9 +3,13 @@
 
 using namespace std;
 
+// Demonstrates the usage of a reference and pointer declaration
 int main() {
     int num {21};
     cout << "Address of num: " << setw(10) << &num << endl;
+
+    int *intptr {};
+    float *floatptr {};
 
     return 0;
 }
