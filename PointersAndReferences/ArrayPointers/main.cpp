6b5570e43d96fd2703a7ce8a87ab8ca90d3f6f13
Troy Martin <beef.erikson.studios@gmail.com>
2020-02-29 00:25:00 -0500

    ArrayPointers updated.

--- PointersAndReferences/ArrayPointers/main.cpp
+++ PointersAndReferences/ArrayPointers/main.cpp
@@ -7,8 +7,11 @@ int main() {
     int list[] {2, 7, 1, 9, 23};
     int *iptr (list);
 
-    for (int i{0}; i < 5; i++)
-        cout << *iptr++ << endl;
-
+    for (int i{0}; i < 5; i++) {
+        *iptr = list[0];
+        for (int j{0}; j < 5; j++)
+            cout << *(iptr + j) << " ";
+        cout << endl;
+    }
     return 0;
 }
