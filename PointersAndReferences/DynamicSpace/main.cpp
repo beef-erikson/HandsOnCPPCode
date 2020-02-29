20b0d44c29dbfc99ade334b9c736014bd46b5fab
Troy Martin <beef.erikson.studios@gmail.com>
2020-02-29 16:55:29 -0500

    DynamicSpace expanded.

--- PointersAndReferences/DynamicSpace/main.cpp
+++ PointersAndReferences/DynamicSpace/main.cpp
@@ -5,8 +5,9 @@ using namespace std;
 // Using dynamic memory allocation
 int main() {
     // Defines pointer and allocates space
-    int *iptr {new int};
-
+    int *iptr {new int(20)};
+    cout << *iptr << endl;
+    
     // Stores value in allocated space
     *iptr = 12;
 
