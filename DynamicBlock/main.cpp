867a8eb082f8f05f40ef81e355dd798e37f1037f
beef-erikson <beef.erikson.studios@gmail.com>
2020-03-01 17:36:41 -0500

    Moved project to linux and added DynamicBlock

--- DynamicBlock/main.cpp
+++ DynamicBlock/main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 
+using namespace std;
+
+// Sets up a memory block of integers
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int *iptr = {new int[4]{}};
+    cout << "Enter 4 integers: ";
+    scanf("%d %d %d %d", &iptr[0], &iptr[1], &iptr[2], &iptr[3]);
+    printf("You have entered: %d %d %d %d", iptr[0], iptr[1], iptr[2], iptr[3]);
+
+    delete[] iptr;
+    iptr = nullptr;
+
     return 0;
 }
